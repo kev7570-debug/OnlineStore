564fb983ea97bb8e663f0b36c9b8e5395d894822
elena <kev7570@gmail.com>
2026-06-23 22:33:05 +0300

    feat: add custom user model with email auth and admin registration

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',  # добавлена строка
     'blog',  # добавлена новая строка
+    'users',  # добавлена новая строка
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+AUTH_USER_MODEL = 'users.User'
