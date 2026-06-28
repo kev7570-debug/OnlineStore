e33bef04e3ed1fc64dece514afd558abb252215a
elena <kev7570@gmail.com>
2026-06-28 19:58:21 +0300

    feat: install Redis and configure caching settings

--- config/settings.py
+++ config/settings.py
@@ -153,3 +153,14 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = 'catalog:home'
 LOGOUT_REDIRECT_URL = 'catalog:home'
 LOGIN_URL = 'users:login'
+
+# Redis caching
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
