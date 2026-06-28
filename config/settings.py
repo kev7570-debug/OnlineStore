35a61c7b7b7439b6cd5b4f00ddfe58791de129b9
elena <kev7570@gmail.com>
2026-06-28 21:02:54 +0300

    feat: add low-level caching for category product list

--- config/settings.py
+++ config/settings.py
@@ -157,10 +157,7 @@ LOGIN_URL = 'users:login'
 # Redis caching
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
     }
 }
